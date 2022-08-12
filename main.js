68804bd5d2b7b7b80729552ed4ec5cd79a0d2536
mawrk <jangomaxstudio@gmail.com>
2022-08-12 00:31:12 -0700

    Gapless Columns

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let height, width, mines
+let board = []
 
 const startGame = () => {
   height = document.getElementById('height').value
@@ -13,14 +14,14 @@ const startGame = () => {
   
   for(let row = 0; row < height; row++){
     let div = document.createElement('div')
-    div.className = 'columns'
+    div.className = 'columns is-gapless'
     div.id = `row${row + 1}`
     
     gameBoard.append(div)
     
     for(let col = 0; col < width; col++){
       let column = document.createElement('div')
-      column.className = 'column is-narrow'
+      column.className = 'column'
       column.id = `column${col + 1}`
       column.style.color = 'black'
 
@@ -29,13 +30,15 @@ const startGame = () => {
     }
   }
 
-  let board = placeRadii(placeMines(initMap(height, width)))
+  initMap(height, width)
+  placeMines()
+  placeRadii()
+
   printBoard(board)
   render(board)
 }
 
 const initMap = () => {
-  let board = []
   for(let i = 0; i < height; i++){
     let line = []
     for(let j = 0; j < width; j++){
@@ -43,32 +46,31 @@ const initMap = () => {
     }
     board.push(line)
   }
-  return board
 }
 
-const placeMines = (board) => {
+const placeMines = () => {
   for(let i = 0; i < mines; i++){
     let ranX = Math.floor(Math.random() * width)
     let ranY = Math.floor(Math.random() * height)
     if(board[ranY][ranX] != -1){
       board[ranY][ranX] = -1
+    } else {
+      i--
     }
   }
-  return board
 }
 
-const placeRadii = (board) => {
+const placeRadii = () => {
   for(let i = 0; i < height; i++){
     for(let j = 0; j < width; j++){
       if(board[i][j] != -1){
-        board[i][j] = checkNeighbors(board, i, j)
+        board[i][j] = checkNeighbors(i, j)
       }
     }
   }
-  return board
 }
 
-const checkNeighbors = (board, x, y) => {
+const checkNeighbors = (x, y) => {
   let count = 0
 /*
  *  [][   ][]
@@ -77,40 +79,40 @@ const checkNeighbors = (board, x, y) => {
  */
   //top left
   if(x > 0 && y > 0){
-    if(isMine(board, x-1, y-1)) count++
+    if(isMine(x-1, y-1)) count++
   }
   //top mid
   if(y > 0){
-    if(isMine(board, x, y-1)) count++
+    if(isMine(x, y-1)) count++
   }
   //top right
   if(x < width-1 && y > 0){
-    if(isMine(board, x+1, y-1)) count++
+    if(isMine(x+1, y-1)) count++
   }
   //mid left
   if(x > 0){
-    if(isMine(board, x-1, y)) count++
+    if(isMine(x-1, y)) count++
   }
   //mid right
   if(x < width-1){
-    if(isMine(board, x+1, y)) count++
+    if(isMine(x+1, y)) count++
   }
   //bot left 
   if(x > 0 && y < height-1){
-    if(isMine(board, x-1, y+1)) count++
+    if(isMine(x-1, y+1)) count++
   }
   //bot mid 
   if(y < height-1){
-    if(isMine(board, x, y+1)) count++
+    if(isMine(x, y+1)) count++
   }
   //bot right
   if(x < width-1 && y < height-1){
-    if(isMine(board, x+1, y+1)) count++
+    if(isMine(x+1, y+1)) count++
   }
   return count
 }
 
-const isMine = (board, x, y) => {
+const isMine = (x, y) => {
   if(board[x][y] == -1){
     return true
   }else{
@@ -118,7 +120,19 @@ const isMine = (board, x, y) => {
   }
 }
 
-const printBoard = (board) => {
+const isEmpty = (x, y) => {
+  if(board[x][y] == 0){
+    return true
+  }else{
+    return false
+  }
+}
+
+const revealMines = () => {
+
+}
+
+const printBoard = () => {
   let out = ''
   for(let i = 0; i < board.length; i++){
     for(let j = 0; j < board[i].length; j++){
@@ -129,15 +143,13 @@ const printBoard = (board) => {
   console.log(out)
 }
 
-const render = (board) => {
+const render = () => {
   for(let i = 0; i < board.length; i++){
     let row = document.getElementById(`row${i+1}`)
     for(let j = 0; j < board[i].length; j++){
       let imgCont = document.createElement('figure')
-      imgCont.className = 'image is-16x16'
+      imgCont.className = 'image is-16x16 is-gapless'
       imgCont.id = `${i}b${j}`
-      //let cell = row.getElementsByClassName(`column`)[j]
-      //cell.appendChild(imgCont)
       let img = document.createElement('img')
 
       if(board[i][j] !== -1){
